fa493472a84a3aa1bb16487a54610717c5b84437
Gabor Csikos <gabor.csikos@lush.co.uk>
2018-07-24 20:43:00 +0100

    let parameterised routes have the same label

--- src/Http/Middleware/MetricsMiddleware.php
+++ src/Http/Middleware/MetricsMiddleware.php
@@ -39,12 +39,37 @@ class MetricsMiddleware
      */
     public function terminate($request, $response)
     {
+        $matchedRoute = $this->getMatchedRoute();
+
         $httpRequestsTotal = $this->collectorRegistry->registerCounter('http', 'requests_total', 'request counter', ['service', 'code', 'path', 'method']);
-        $httpRequestsTotal->incBy(1, [env('SERVICE_NAME'), $response->status(), '/' . $request->path(), $request->method()]);
+        $httpRequestsTotal->incBy(1, [env('SERVICE_NAME'), $response->status(), $matchedRoute, $request->method()]);
 
         $requestDuration = round((microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']) * 1000, 2);
         $httpRequestDurationMilliseconds = $this->collectorRegistry->registerHistogram('http', 'request_duration_milliseconds', 'histogram for better visiblity of service performance', ['service', 'code', 'path', 'method'], [50, 80, 100, 120, 150, 200, 300, 500, 800, 1000, 2000, 5000]);
-        $httpRequestDurationMilliseconds->observe($requestDuration, [env('SERVICE_NAME'), $response->status(), '/' . $request->path(), $request->method()]);
+        $httpRequestDurationMilliseconds->observe($requestDuration, [env('SERVICE_NAME'), $response->status(), $matchedRoute, $request->method()]);
     }
 
+    /**
+     * Protected currentRoute is not too handy :(
+     *
+     * @return string
+     */
+    private function getMatchedRoute()
+    {
+        $change = function () {
+            return $this->currentRoute;
+        };
+
+        $controller = $change->call(app())[1]['uses'];
+
+        $matchedRoute = 'unknown';
+        foreach (app()->getRoutes() as $route) {
+            if ($controller == $route['action']['uses']) {
+                $matchedRoute = $route['uri'];
+                break;
+            }
+        }
+
+        return $matchedRoute;
+    }
 }
